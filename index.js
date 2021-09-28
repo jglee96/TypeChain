fe8ffd00681a3bf8822ef91ccc79ae8f6b45a794
jglee96 <zakklee96@gmail.com>
2021-09-28 22:44:38 +0900

    First Typescript
    * typescript에서는 정해진 규칙대로 코드를 작성하지 않으면 디비거 이를 걸러내어 js로 변환되지 않음
    * 함수 정의시 변수 옆에 ?를 붙이면 선택적인 변수로 변하여 값을 넣지 않아도 undefined로 실행됨

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-console.log("hello");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "Zakk", age = 26, gender = "male";
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age}, your a ${gender}`);
+};
+sayHi(name, age, gender);
+sayHi(name, age);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("hello");
+const name = "Zakk",
+    age = 26,
+    gender = "male"
+
+const sayHi = (name, age, gender?) => {
+    console.log(`Hello ${name}, you are ${age}, your a ${gender}`);
+}
+
+sayHi(name, age, gender);
+sayHi(name, age);
+
+export {};
