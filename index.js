0578dd84868935fc2162b34a2be357856ee24518
jglee96 <zakklee96@gmail.com>
2021-11-14 22:11:00 +0900

    [feat] typescript로 block chain 만들기 완성
    * type scipt의 장점을 이용하여 block chain 만들기
    * class, static function, interface 활용

--- index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const name = "Zakk", age = 26, gender = "male";
-const sayHi = (name, age, gender) => {
-    console.log(`Hello ${name}, you are ${age}, your a ${gender}`);
-};
-sayHi(name, age, gender);
-sayHi(name, age);
-//# sourceMappingURL=index.js.map
